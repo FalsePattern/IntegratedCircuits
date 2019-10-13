56072d62ba699c3e23ef309d7d6042722369022d
FalsePattern <thefalsepattern@gmail.com>
2019-10-13 17:15:25 +0200

    Comparator hotfix - 32-bit values now unsigned

--- Components/Cmp/CmpBase.cs
+++ Components/Cmp/CmpBase.cs
@@ -11,8 +11,8 @@ namespace IntegratedCircuits.Components.Cmp
         }
         protected override void CircuitLogicUpdate()
         {
-            int a = Util.ReadIntFromInputs(Inputs, 0, bits - 1);
-            int b = Util.ReadIntFromInputs(Inputs, bits, 2 * bits - 1);
+            uint a = (uint)Util.ReadIntFromInputs(Inputs, 0, bits - 1);
+            uint b = (uint)Util.ReadIntFromInputs(Inputs, bits, 2 * bits - 1);
             Outputs[0].On = a < b;
             Outputs[1].On = a == b;
             Outputs[2].On = a > b;
